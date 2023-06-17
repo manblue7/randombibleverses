cae96da92e79c38aae472af2611196a92d67b731
Carder D <45611354+manblue7@users.noreply.github.com>
2023-06-16 17:36:40 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,51 @@
+import bible from './kjv.json' assert { type: 'json' };
+
+
+document.getElementById('ver').addEventListener('click', generateVerse);
+document.getElementById('ps').addEventListener('click', generatePsalm);
+document.getElementById('pro').addEventListener('click', generatePsalm);
+function generateVerse() {
+    let vNum = Math.floor(Math.random() * bible["verses"].length);
+    let verses = bible["verses"][vNum];
+    let verse = verses["text"];
+    let verseInfo = verses["book_name"] +
+    " " + verses["chapter"] + ":" + verses["verse"];
+    document.getElementById("verse").innerHTML = verseInfo + " " + verse;
+    console.log("generated");
+}
+
+function generatePsalm() {
+    while(true) {
+    let vNum = Math.floor(Math.random() * bible["verses"].length);
+    let verses;
+    if(bible["verses"][vNum]["book_name"] == "Psalms") {
+        verses = bible["verses"][vNum]
+    } else {
+        continue;
+    }
+    let verse = verses["text"];
+    let verseInfo = verses["book_name"] +
+    " " + verses["chapter"] + ":" + verses["verse"];
+    document.getElementById("verse").innerHTML = verseInfo + " " + verse;
+    console.log("generated");
+    break;
+    } 
+}
+
+function generateProverb() {
+    while(true) {
+    let vNum = Math.floor(Math.random() * bible["verses"].length);
+    let verses;
+    if(bible["verses"][vNum]["book_name"] == "Proverb") {
+        verses = bible["verses"][vNum]
+    } else {
+        continue;
+    }
+    let verse = verses["text"];
+    let verseInfo = verses["book_name"] +
+    " " + verses["chapter"] + ":" + verses["verse"];
+    document.getElementById("verse").innerHTML = verseInfo + " " + verse;
+    console.log("generated");
+    break;
+    } 
+}
