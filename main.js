9c9da32ebb9ad390f6506ba974517d5d791c85dd
Carder D <45611354+manblue7@users.noreply.github.com>
2023-06-16 22:04:31 -0700

    Update main.js two verses per click

--- main.js
+++ main.js
@@ -5,6 +5,22 @@ document.getElementById('pro').addEventListener('click', generateProverb);
 function generateVerse() {
     let vNum = Math.floor(Math.random() * bible["verses"].length);
     let verses = bible["verses"][vNum];
+    let verses2 = "";
+    if(bible["verses"][vNum - 1]["book_name"] == bible["verses"][vNum]["book_name"]) {
+        verses2 = bible["verses"][vNum - 1];
+    }
+    else {
+        document.getElementById("verse").innerHTML = "";
+    }
+    
+    let verseInfo2;
+    if(verses2 != "") {
+        let verse2 = verses2["text"];
+        verseInfo2 = verses2["book_name"] +
+        " " + verses2["chapter"] + ":" + verses2["verse"];
+        document.getElementById("verseB").innerHTML = verseInfo2 + " " + verse2;
+        console.log("generated");
+    }
     let verse = verses["text"];
     let verseInfo = verses["book_name"] +
     " " + verses["chapter"] + ":" + verses["verse"];
@@ -16,10 +32,24 @@ function generatePsalm() {
     while(true) {
     let vNum = Math.floor(Math.random() * bible["verses"].length);
     let verses;
+    let verses2 = "";
     if(bible["verses"][vNum]["book_name"] == "Psalms") {
-        verses = bible["verses"][vNum]
+        verses = bible["verses"][vNum];
+        if(bible["verses"][vNum - 1]["book_name"] == "Psalms") {
+            verses2 = bible["verses"][vNum - 1];
+        }
+        
     } else {
+        document.getElementById("verse").innerHTML = "";
         continue;
+    }
+    let verseInfo2;
+    if(verses2 != "") {
+        let verse2 = verses2["text"];
+        verseInfo2 = verses2["book_name"] +
+        " " + verses2["chapter"] + ":" + verses2["verse"];
+        document.getElementById("verseB").innerHTML = verseInfo2 + " " + verse2;
+        console.log("generated");
     }
     let verse = verses["text"];
     let verseInfo = verses["book_name"] +
@@ -34,10 +64,24 @@ function generateProverb() {
     while(true) {
     let vNum = Math.floor(Math.random() * bible["verses"].length);
     let verses;
+    let verses2 = "";
     if(bible["verses"][vNum]["book_name"] == "Proverbs") {
-        verses = bible["verses"][vNum]
+        verses = bible["verses"][vNum];
+        if(bible["verses"][vNum - 1]["book_name"] == "Proverbs") {
+            verses2 = bible["verses"][vNum - 1];
+        }
+        
     } else {
+        document.getElementById("verse").innerHTML = "";
         continue;
+    }
+    let verseInfo2;
+    if(verses2 != "") {
+        let verse2 = verses2["text"];
+        verseInfo2 = verses2["book_name"] +
+        " " + verses2["chapter"] + ":" + verses2["verse"];
+        document.getElementById("verseB").innerHTML = verseInfo2 + " " + verse2;
+        console.log("generated");
     }
     let verse = verses["text"];
     let verseInfo = verses["book_name"] +
